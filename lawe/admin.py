b4e740b2e3aa267916d4e1ba7e23a2e391c63aee
Andrey Valyaev <dron.valyaev@gmail.com>
2016-12-26 23:00:47 +0300

    #2: Непрерывная интеграция и стиль кода

--- lawe/admin.py
+++ lawe/admin.py
@@ -1,3 +1,1 @@
-from django.contrib import admin
-
-# Register your models here.
+''' Register your models here. '''
--- lawe/apps.py
+++ lawe/apps.py
@@ -1,5 +1,7 @@
+''' Apps '''
 from django.apps import AppConfig
 
 
 class LaweConfig(AppConfig):
-    name = 'lawe'
+	''' LaweConfig '''
+	name = 'lawe'
