b796b2a72f7655c29e88ebfc416d76801526b261
Andrey Valyaev <dron.valyaev@gmail.com>
2017-01-02 20:23:44 +0300

    #4: Формирование содержимого ответа, не завершено

--- lawe/tests/testOperations.py
+++ lawe/tests/testOperations.py
@@ -1,5 +1,6 @@
 ''' Operation tests '''
 from django.test import Client, TestCase
+from lawe.models import Account, Transaction
 
 
 class TestOperations(TestCase):
@@ -11,3 +12,17 @@ class TestOperations(TestCase):
 		response = Client().get('/')
 		# Then
 		self.assertEqual(response.status_code, 200)
+
+	def testPageContainLastOperation(self):
+		''' Последняя введенная операция отображается в окне операций '''
+		# Given
+		a1 = Account.objects.create(group='G', subgroup='E', name='N', shortname='S',
+				unit='тр')
+		a2 = Account.objects.create(group='G', subgroup='E', name='NN', shortname='SN',
+				unit='тр')
+		Transaction.objects.create(debit=a1, credit=a2, amount=432, description='')
+		# When
+		response = Client().get('/')
+		# Then
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('432', response.content.decode('utf8'))
--- style.sh
+++ style.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-find ./ -name "*.py" | xargs pep8 --show-source --max-line-length=100 --ignore=W191
+find ./ -name "*.py" | xargs pep8 --show-source --max-line-length=100 --ignore=W191,E128
 find ./ -name "*.py" | xargs -r pylint
