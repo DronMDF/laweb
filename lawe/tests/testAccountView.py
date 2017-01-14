a2a453c0752ce76893177ca69c370193f84965c1
Andrey Valyaev <dron.valyaev@gmail.com>
2017-01-14 22:53:22 +0300

    #14: Список операци аккаунта в результирующем xml

--- lawe/tests/testAccountView.py
+++ lawe/tests/testAccountView.py
@@ -5,6 +5,11 @@ from lawe.models import Account, Transaction
 
 class TestAccountView(TestCase):
 	''' Тестирование AccountView '''
+	def setUp(self):
+		''' Другой аккаунт мы создадим заранее, чтобы он не замусоривал тесты '''
+		self.oacc = Account.objects.create(group='G', subgroup='E', name='N', shortname='S',
+				unit='тр')
+
 	def testZeroAccount(self):
 		''' Аккаунт без транзакций выдает сумму 0 '''
 		# Given
@@ -21,11 +26,23 @@ class TestAccountView(TestCase):
 		# Given
 		acc = Account.objects.create(group='G', subgroup='E', name='N', shortname='S',
 				unit='тр')
-		a2 = Account.objects.create(group='G', subgroup='E', name='N', shortname='S',
-				unit='тр')
-		Transaction.objects.create(debit=a2, credit=acc, amount=100, description='')
+		Transaction.objects.create(debit=self.oacc, credit=acc, amount=100, description='')
 		# When
 		response = Client().get('/account/%u' % acc.id)
 		# Then
 		self.assertEqual(response.status_code, 200)
 		self.assertIn('<total>100</total>', response.content.decode('utf8'))
+		self.assertIn('<income>100</income>', response.content.decode('utf8'))
+
+	def testDebitAccount(self):
+		''' Со счетасписаны  деньги '''
+		# Given
+		acc = Account.objects.create(group='G', subgroup='E', name='N', shortname='S',
+				unit='тр')
+		Transaction.objects.create(debit=acc, credit=self.oacc, amount=100, description='')
+		# When
+		response = Client().get('/account/%u' % acc.id)
+		# Then
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('<total>-100</total>', response.content.decode('utf8'))
+		self.assertIn('<outcome>100</outcome>', response.content.decode('utf8'))
